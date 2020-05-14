650e4c45fad2ace431494eddc2690a8c9c0c4090
Christian Chostak <christian.chostak@devoteam.com>
2020-05-14 22:22:07 +0100

    WIP: Added ec2 to s3

--- Terraform/Terraform-EC2-S3/main.tf
+++ Terraform/Terraform-EC2-S3/main.tf
@@ -110,3 +110,40 @@ module "s3_bucket" {
   }
 
 }
+
+# https://github.com/terraform-aws-modules/terraform-aws-elb
+module "elb" {
+  source = "terraform-aws-modules/elb/aws"
+
+  name = "DevOps-elb"
+
+  subnets         = module.vpc.public_subnets
+  security_groups = [module.ssh_service_sg.this_security_group_id]
+  internal        = false
+
+  listener = [
+    {
+      instance_port     = "80"
+      instance_protocol = "HTTP"
+      lb_port           = "80"
+      lb_protocol       = "HTTP"
+    },
+  ]
+
+  health_check = {
+    target              = "HTTP:80/"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+  }
+
+  access_logs = {
+    bucket = module.s3_bucket.this_s3_bucket_id
+  }
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+}
